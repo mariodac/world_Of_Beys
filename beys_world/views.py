bf1421ef897ffaeb5f380542ca2affdb0c98f5eb
mariodac <mdac.mario@gmail.com>
2025-03-13 09:48:17 -0300

    Buscando dados da base dados

--- beys_world/views.py
+++ beys_world/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
+from beys_world.models import Blade
+
 def index(request):
+    blades = Blade.objects.all()
+    context = {
+        'blades': blades
+    }
     return render(
         request,
         'beys_world/index.html',
+        context,
     )
