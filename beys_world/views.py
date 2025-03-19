460f1f19434c61fb1669950b9dc6e779f30f669e
mariodac <mdac.mario@gmail.com>
2025-03-18 21:34:28 -0300

    Adiciona mais variáveis ao contexto

--- beys_world/views.py
+++ beys_world/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 
-from beys_world.models import Blade
+from beys_world.models import AssistBlade, Bit, Blade, LockChip, MainBlade, Ratchet
 
 def index(request):
     blades = Blade.objects.all()
+    ratchets = Ratchet.objects.all()
+    bits = Bit.objects.all()
+    lock_chips = LockChip.objects.all()
+    main_blades = MainBlade.objects.all()
+    assist_blades = AssistBlade.objects.all()
     context = {
-        'blades': blades
+        'blades': blades,
+        'ratchets': ratchets,
+        'bits': bits,
+        'lock_chips': lock_chips,
+        'main_blades': main_blades,
+        'assist_blades': assist_blades
     }
     return render(
         request,
