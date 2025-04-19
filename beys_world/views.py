b4031d0f61fe62be19a28be9407c13e30f6fa65a
mariodac <mdac.mario@gmail.com>
2025-04-19 01:45:53 -0300

    Update views.py
    Adicionado filtro para retorna apenas os que estão habilitados para exibição

--- beys_world/views.py
+++ beys_world/views.py
@@ -3,12 +3,12 @@ from django.shortcuts import render
 from beys_world.models import AssistBlade, Bit, Blade, LockChip, MainBlade, Ratchet
 
 def index(request):
-    blades = Blade.objects.all()
-    ratchets = Ratchet.objects.all()
-    bits = Bit.objects.all()
-    lock_chips = LockChip.objects.all()
-    main_blades = MainBlade.objects.all()
-    assist_blades = AssistBlade.objects.all()
+    blades = Blade.objects.all().filter(show=True)
+    ratchets = Ratchet.objects.all().filter(show=True)
+    bits = Bit.objects.all().filter(show=True)
+    lock_chips = LockChip.objects.all().filter(show=True)
+    main_blades = MainBlade.objects.all().filter(show=True)
+    assist_blades = AssistBlade.objects.all().filter(show=True)
     context = {
         'blades': blades,
         'ratchets': ratchets,
