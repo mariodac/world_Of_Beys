3903380cdf5209c5c70228a8091234229590c182
mariodac <mdac.mario@gmail.com>
2025-06-13 03:50:39 -0300

    adicionado rota all_parts

--- beys_world/urls.py
+++ beys_world/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 
 from beys_world import views
 
-app_name = 'beys world'
+app_name = 'beys_world'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('all_parts/', views.all_parts, name='all_parts'),
 ]
--- beys_world/views.py
+++ beys_world/views.py
@@ -3,19 +3,21 @@ from django.shortcuts import render
 from beys_world.models import AssistBlade, Bit, Blade, LockChip, MainBlade, Ratchet
 
 def index(request):
-    blades = Blade.objects.all().filter(show=True)
-    ratchets = Ratchet.objects.all().filter(show=True)
-    bits = Bit.objects.all().filter(show=True)
-    lock_chips = LockChip.objects.all().filter(show=True)
-    main_blades = MainBlade.objects.all().filter(show=True)
-    assist_blades = AssistBlade.objects.all().filter(show=True)
-    parts = [blades, ratchets, bits, lock_chips, main_blades, assist_blades]
     context = {
-        'all_parts': parts,
-        'title': 'index',
+        "title": "Home",
     }
     return render(
         request,
         'beys_world/index.html',
-        context,
+        context
+    )
+
+def all_parts(request):
+    context = {
+        "title": "All Parts",
+    }
+    return render(
+        request,
+        'beys_world/all_parts.html',
+        context
     )
